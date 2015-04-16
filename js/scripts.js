218cec77e98656a342df69d5dedcbd1e20d2adee
virginie trubiano & ericacha <student@epicodus.com>
2015-04-16 12:17:31 -0700

    click function() not displaying the task.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,41 @@
+
+
 $(document).ready(function() {
     $("form#list").submit(function(event){
         event.preventDefault();
 
-    var inputtedTitle = $("input#new-title").val();
-    newTitle = { name : inputtedTitle, tasks :[]};
+        var inputtedTitle = $("input#new-title").val();
+        var newTitle = { name : inputtedTitle, tasks :[]};
 
-    $('ul#lists').append('<li><span class="list">' + newTitle.name + '</span></li>');
+        $('ul#lists').append('<li><span class="list">' + newTitle.name + '</span></li>');
 
-    $("input#new-title").val('');
+
+        $("input#new-title").val('');
 
     });
+
+
+    $('ul#lists').last().click(function() {
+        $('#task_page').show();
+        $('#task_page h3').text(newTitle.name);
+
+        $("form#task").submit(function(event) {
+            event.preventDefault();
+
+            var inputtedTask = $("input#description").val();
+            var newTask = {description : inputtedTask };
+            newTitle.tasks.push(newTask);
+
+            $("ul.tasks").text("");
+            newTitle.tasks.forEach(function(task) {
+                $('ul.tasks').append('<li>' + task.description + '</li>');
+            });
+
+            $("input#description").val('');
+        });
+
+    });
+
+
+
 });
