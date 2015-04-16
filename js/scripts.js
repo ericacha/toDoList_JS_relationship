4b866ac2529c241a4bf7228252e9db9243ed5795
virginie trubiano & ericacha <student@epicodus.com>
2015-04-16 16:31:29 -0700

    edit comment.

--- js/scripts.js
+++ js/scripts.js
@@ -20,42 +20,41 @@ $(document).ready(function() {
             //after the click, show the task page
             $('#task_page').show();
 
-            //deletes what the user entered in the value box
+            //deletes what the user entered previously
             $("ul.tasks").text("");
 
             //the list of name selected shows on the task page in size h4
             $('#task_page h4').text(newTitle.name);
 
-            //goes through the tasks in the newTitle and lists description
+            //goes through the tasks from newTitle and list the description
             newTitle.tasks.forEach(function(task) {
                 $('ul.tasks').append('<li>' + task.description + '</li>');
 
-            //close
             });
 
-            //
+            //off method prevents it from submitting several times , as we are still in the submit function above
             $("form#task").off('submit');
+            //new form for task
             $("form#task").submit(function(event) {
                 event.preventDefault();
-
+                //user enters a task and saves it as newTask variable
                 var inputtedTask = $("input#description").val();
                 var newTask = {description : inputtedTask };
+                //pushes newTask to the tasks array in newTitle
                 newTitle.tasks.push(newTask);
 
+                //deletes what the user entered previously
                 $("ul.tasks").text("");
+                //goes through the tasks from newTitle and list the description
                 newTitle.tasks.forEach(function(task) {
                     $('ul.tasks').append('<li>' + task.description + '</li>');
                 });
-
+                //deletes what the user entered previously
                 $("input#description").val('');
             });
 
         });
 
     });
-
-
-
-
 
 });
