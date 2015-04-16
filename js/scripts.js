c2d6305040a7666c37848d92cc64de6cb1959f53
virginie trubiano & ericacha <student@epicodus.com>
2015-04-16 16:24:35 -0700

    add comments.

--- js/scripts.js
+++ js/scripts.js
@@ -4,28 +4,36 @@ $(document).ready(function() {
     $("form#list").submit(function(event){
         event.preventDefault();
 
+        //user enters title value and then saves it as newTitle
         var inputtedTitle = $("input#new-title").val();
         var newTitle = { name : inputtedTitle, tasks :[]};
 
+        //append the list of names user enters
         $('ul#lists').append('<li><span class="list">' + newTitle.name + '</span></li>');
 
-
+        //deletes what the user entered in the value box
         $("input#new-title").val('');
 
 
-
+        //the last click along with its information is included
         $('.list').last().click(function() {
+            //after the click, show the task page
             $('#task_page').show();
 
+            //deletes what the user entered in the value box
             $("ul.tasks").text("");
 
+            //the list of name selected shows on the task page in size h4
             $('#task_page h4').text(newTitle.name);
-            debugger;
 
+            //goes through the tasks in the newTitle and lists description
             newTitle.tasks.forEach(function(task) {
                 $('ul.tasks').append('<li>' + task.description + '</li>');
+
+            //close
             });
 
+            //
             $("form#task").off('submit');
             $("form#task").submit(function(event) {
                 event.preventDefault();
